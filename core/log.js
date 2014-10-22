09be1c57ea9838f5fa7e951162d9db695b2bf6be
boo1ean <boo1ean0807@gmail.com>
2014-10-22 22:26:22 +0300

    Improve logger a lot

--- core/log.js
+++ core/log.js
@@ -1,15 +1,60 @@
 var PrettyError = require('pretty-error');
+var _ = require('lodash');
+
 var pe = new PrettyError();
 
-var notify = function(level) {
-	return console.log.bind(console, level);
+function scalars_only (item) {
+	return !_.isObject(item);
 };
 
-var notify_error = function(level) {
-	return function(error) {
-		console.error(level);
+function objects_only (item) {
+	return _.isObject(item);
+};
+
+function get_data (args) {
+	args = args.filter(objects_only);
+
+	if (args.length === 1) {
+		return args[0];
+	}
+
+	return args;
+};
+
+function get_message (args) {
+	return args.filter(scalars_only).join('');
+};
+
+function current_time () {
+	return new Date();
+};
+
+function notify (level) {
+	return function() {
+		var args = Array.prototype.slice.apply(arguments);
+
+		return console.log({
+			level: level,
+			time: current_time(),
+			message: get_message(args),
+			data: get_data(args)
+		});
+	};
+};
+
+var find_error = function(args) {
+	return _.find(args, _.isObject);
+};
+
+function notify_error (level) {
+	return function() {
+		var args = Array.prototype.slice.apply(arguments);
+
+		var message = get_message(args);
+		var error = find_error(args);
+
 		var rendered_error = pe.render(error);
-		console.error(rendered_error);
+		console.error(message, rendered_error);
 	};
 };
 
@@ -73,6 +118,6 @@ var log = {
 
 module.exports = log;
 
-process.on('uncaughtException', function (error) {
-	log.alert('Uncaught exception', error);
+process.on('uncaughtException', function onUncaughtException (error) {
+	log.alert(error);
 });
