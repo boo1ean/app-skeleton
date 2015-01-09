b475080349650422cc5bd3f90e3c1dd6c469321c
boo1ean <boo1ean0807@gmail.com>
2015-01-09 15:15:34 +0200

    Remove default logging from service

--- framework/service.js
+++ framework/service.js
@@ -1,10 +1,3 @@
 var service = require('app-service');
-var log = require('./log');
-
-function before (methodName) {
-	log.debug(this.methodName, arguments);
-}
-
-service.configure({ before: before });
 
 module.exports = service;
