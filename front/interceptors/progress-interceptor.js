683ac059496a5bd4abf7fc72b7ab804aaf2414bf
boo1ean <boo1ean0807@gmail.com>
2015-02-01 20:58:33 +0200

    Fix progress interceptor

--- front/interceptors/progress-interceptor.js
+++ front/interceptors/progress-interceptor.js
@@ -1,23 +1,34 @@
 angular.module('app.services').factory('progressInterceptor',
-   ['$rootScope', '$timeout', '$q', '$location', 'config',
-	function ($q) {
+   ['$q', function ($q) {
 		var progressElement = $('.progress-indicator');
 		var depth = 0;
 
 		return {
-			request: function onRequest (config) {
-				depth++;
-				progressElement.removeClass('hide');
-				return config;
-			},
-
-			response: function onResponse (response) {
-				if (--depth === 0) {
-					progressElement.addClass('hide');
-				}
-
-				return response || $q.when(response);
-			}
+			request: onRequest,
+			response: onResponse,
+			responseError: onResponseError
 		};
+
+		function onRequest (config) {
+			depth++;
+			progressElement.removeClass('hide');
+			return config;
+		}
+
+		function onResponse (response) {
+			if (--depth === 0) {
+				progressElement.addClass('hide');
+			}
+
+			return response || $q.when(response);
+		}
+
+		function onResponseError (rejection) {
+			if (--depth === 0) {
+				progressElement.addClass('hide');
+			}
+
+			return $q.reject(rejection);
+		}
 	}
 ]);
