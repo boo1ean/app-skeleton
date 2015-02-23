270376c0dec4e9305899d9c04c54f249291a4a3a
boo1ean <boo1ean0807@gmail.com>
2015-02-23 15:07:12 +0200

    Fix sample migration

--- database/migrations/20141203211559_create_users_table.js
+++ database/migrations/20141203211559_create_users_table.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
 	return knex.schema.createTable('users', function(t) {
 		t.bigIncrements().primary();
 
-		t.dateTime('created_at').notNull().defaultTo('now');
+		t.dateTime('created_at').notNull().defaultTo(knex.raw('now'));
 		t.dateTime('updated_at').nullable();
 
 		t.text('name').unique().notNull();
