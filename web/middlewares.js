92ca328b4460e9c30280dc430bb89b2aba77ae0b
boo1ean <boo1ean0807@gmail.com>
2015-03-02 16:26:46 +0200

    Add helmet

--- web/middlewares.js
+++ web/middlewares.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var helmet = require('helmet');
 var bodyParser = require('body-parser');
 
 var config = require('../config');
 
 module.exports = function configureMiddlewares (app) {
-	app.use(bodyParser.json());
+	app.use(helmet());
 	app.use(express.static(__dirname + '/../static'));
+	app.use(bodyParser.json());
 };
